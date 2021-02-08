c0afa166292cc8ba1fa11fa1727b962d1dac1322
samuel <samuel@familjenlarsson.eu>
2021-02-08 12:07:19 +0100

    update readme and improved quicksort

--- README.md
+++ README.md
@@ -7,15 +7,19 @@
 1. **Compute Sum, Min and Max of Matrix Elements** 
 2. **Quicksort:** Completed
 
-	Currently slightly to slow due to creating too many treads. To improve a threadpool should be created.
+	Currently slightly to slow due to creating too many treads. To improve a threadpool could be implemented.
+	- Performance: With 50000 items in an array we average ~0.64s in runtime. With 100000 items we average ~2.5s in runtime. 2.5/0.64 = ~3.9, this is... not great. It's not terrible but as we should see slightly more than 2 (maybe 2.4 at most) we deperatly need a thread pool or something else that prevents the overhead from creating thousands of threads.
+	- Improved perfomance: After altering the code so a thread only creates one new thread after a completed quicksort step (and does the other half itself) the performance has nearly doubled.
 3. **Compute pi:** Completed
 
-	Implemented by multiplying the ratio of randomly generated points (x and y generated to be between 0 and 1) that have a distance of less than 1 from (0, 0) and total generated points by 4. Tends slightly to 3.12?
+	Implemented by multiplying the ratio of randomly generated points (x and y generated to be between 0 and 1) that have a distance of less than 1 from (0, 0) and total generated points by 4. Tends slightly to 3.12 strangely?
+	- Performance: Very quick, only at 10000000 points do we spend more than 1s before we get results.
 4. **The Unix tee command:** Completed
 
 	Implemented by having main thread read  stdIn char by char and sending them though pipes to stdOut printing thread and file printing thread. There was an issue when setting a file as stdIn where the program closed before both pipes had finished reading and printing. To handle this we end up with a situation where not all threads close leaving us hanging but doing nothing.
 
 	Due to issues with file printing the system I ended up implementing was very inefficient. Having to open and close the file for every character. When not implemented this way the file ended up either not printing or printing ~9 random chars for every char ment to be printed.
+	- Performance: Due to the issues with file printing the file printing thread ends up being somewhat slow. It's fast enough that a human typing would never notice however.
 5. **The Unix diff command**
 6. **Find Palindromic Words**
 7. **The 8-queens problem**
---
+++ hw1/quickSortImproved.c
@@ -0,0 +1,183 @@
+#ifndef _REENTRANT
+#define _REENTRANT
+#endif
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+#include <sys/time.h>
+#include <unistd.h>
+#define MAXSIZE 1000000
+#define MAXRANDOM 100
+#define SWITCHSORT 15 /* should be between 5 and 15 */
+
+typedef struct workerData {
+  int lo;
+  int hi;
+} workerData;
+
+/* timer */
+double read_timer() {
+	static bool initialized = false;
+	static struct timeval start;
+	struct timeval end;
+	if (!initialized) {
+		gettimeofday(&start, NULL);
+		initialized = true;
+	}
+	gettimeofday(&end, NULL);
+	return (end.tv_sec - start.tv_sec) + 1.0e-6 * (end.tv_usec - start.tv_usec);
+}
+
+int size; // size of array
+int array[MAXSIZE];
+pthread_attr_t attr;
+pthread_mutex_t safe;
+pthread_cond_t go;
+
+volatile int workersRunning = 0;
+void startWorker() {
+  pthread_mutex_lock(&safe);
+  workersRunning++;
+  pthread_mutex_unlock(&safe);
+}
+void endWorker(void* data) {
+  free(data);
+  pthread_mutex_lock(&safe);
+  workersRunning--;
+  if(workersRunning <= 0) pthread_cond_broadcast(&go);
+  pthread_mutex_unlock(&safe);
+}
+
+workerData* workerSetup(int lo, int hi) {
+  workerData* this = malloc(sizeof(workerData));
+  this -> lo = lo;
+  this -> hi = hi;
+  return this;
+}
+
+void printArray(signed char f) {
+  if(f < 0) {
+    printf("[ ");
+    for(int i = 0; i < size; i++) {
+      printf("%d", array[i]);
+      if(i != size - 1) printf(", ");
+    }
+    printf(" ]\n");
+  }
+  else if(f) {
+    FILE *fptr = fopen("start", "w+");
+    for(int i = 0; i < size; i++) {
+      fprintf(fptr, "%7d %7d", i, array[i]);
+      fprintf(fptr, ",\n");
+    }
+  }
+  else {
+    FILE *fptr = fopen("end", "w+");
+    for(int i = 0; i < size; i++) {
+      fprintf(fptr, "%7d %7d", i, array[i]);
+      fprintf(fptr, ",\n");
+    }
+  }
+}
+
+unsigned char evalSort() {
+  for(int i = 0; i < size - 1; i++) {
+    if(array[i] > array[i + 1]) {
+      return 0;
+    }
+  }
+  return 1;
+}
+
+void* Worker(void*);
+
+int main(int argc, char* argv[]) {
+  /* set global thread attributes */
+  double start_time, end_time;
+  pthread_attr_init(&attr);
+  pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM);
+  pthread_mutex_init(&safe, NULL);
+  pthread_cond_init(&go, NULL);
+  srand(time(NULL));
+
+  size = (argc > 1) ? atoi(argv[1]) : MAXSIZE;
+	if (size > MAXSIZE) size = MAXSIZE;
+  int maxRand = (argc > 2) ? atoi(argv[2]) : MAXRANDOM;
+
+  for(int i = 0; i < size; i++)
+    array[i] = rand() % maxRand;
+  printArray(-1);
+
+  // Run sort
+  start_time = read_timer();
+
+  startWorker();
+  pthread_t thread;
+  pthread_create(&thread, &attr, Worker, workerSetup(0, size - 1));
+  pthread_cond_wait(&go, &safe);
+
+  end_time = read_timer();
+  // End sort
+
+  printArray(-1);
+  if(evalSort()) printf("Array is verified sorted\n");
+  else printf("Array could not be verified as sorted\n");
+  printf("The execution time is %g sec\n", end_time - start_time);
+}
+
+void shortSwap(int x, int y) {
+  int temp = array[x];
+  array[x] = array[y];
+  array[y] = temp;
+}
+
+void insertionSort(int lo, int hi) {
+  for(int i = lo + 1; i <= hi; i++) {
+    // printf("%d\n", i);
+    int x = array[i];
+    int j;
+    for(j = i - 1; j >= lo && array[j] > x; j--)
+      array[j + 1] = array[j];
+    array[j + 1] = x;
+  }
+}
+// returns pivot
+int quickSort(int lo, int hi) {
+  int mid = (lo + hi) / 2;
+  if(array[mid] < array[lo])
+    shortSwap(mid, lo);
+  if(array[hi] < array[lo])
+    shortSwap(hi, lo);
+  if(array[mid] < array[hi])
+    shortSwap(mid, hi);
+  int pivot = array[hi];
+  int i = lo;
+  for(int j = lo; j <= hi; j++) {
+    if(array[j] < pivot) {
+      shortSwap(i, j);
+      i++;
+    }
+  }
+  shortSwap(i, hi);
+  return i;
+}
+
+void* trash;
+void* Worker(void* arg) {
+  workerData* data = (workerData*) arg;
+run:
+  if(data -> hi <= data -> lo + SWITCHSORT)
+    insertionSort(data -> lo, data -> hi);
+  else {
+    int pivot = quickSort(data -> lo, data -> hi);
+    startWorker();
+    pthread_t upper;
+    pthread_create(&upper, &attr, Worker, workerSetup(pivot + 1, data -> hi));
+    data -> hi = pivot - 1;
+    goto run;
+  }
+  endWorker(data);
+  return trash;
+}
